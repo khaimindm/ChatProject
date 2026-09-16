943024f0ee9578dcc02e4cb35ca16894ac0bdf77
Dmitrii Khaimin <khaimindm@gmail.com>
2026-09-16 21:18:08 +0400

    Start

---
+++ src/main/java/ru/khaimin/chatProject/WebSocket.java
@@ -0,0 +1,32 @@
+package ru.khaimin.chatProject;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.websocket.*;
+import jakarta.websocket.server.PathParam;
+import jakarta.websocket.server.ServerEndpoint;
+
+@ApplicationScoped
+@ServerEndpoint("/chat/{chatId}")
+public class WebSocket {
+
+    @OnOpen
+    public void onOpen(Session session, @PathParam("chatId") String chatId) {
+        System.out.println("onOpen> " + chatId);
+    }
+
+    @OnClose
+    public void onClose(Session session, @PathParam("chatId") String chatId) {
+        System.out.println("onClose> " + chatId);
+    }
+
+    @OnError
+    public void onError(Session session, @PathParam("chatId") String chatId, Throwable throwable) {
+        System.out.println("onError> " + chatId + ": " + throwable);
+    }
+
+    @OnMessage
+    public void onMessage(Session session, @PathParam("chatId") String chatId, String message) {
+        System.out.println("onMessage> " + chatId + ": " + message);
+    }
+
+}
